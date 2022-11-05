6115bcf08d04bbea54d1b05c944ae57bf15e519e
stevewang <privacyalwaysfirst@proton.me>
2022-11-05 15:06:20 -0700

    added ball class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Ball ball = new Ball();
+        Ball ball2 = new Ball();
         System.out.println(ball.name);
+        System.out.println(ball2.name);
     }
 }
