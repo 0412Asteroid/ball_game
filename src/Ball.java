eef4fe6d310a6e68eb83341dc8b9e7cf33ee254a
stevewang <privacyalwaysfirst@proton.me>
2022-11-05 15:24:04 -0700

    added hitpoints and sayHi function to Ball class

--- src/Ball.java
+++ src/Ball.java
@@ -1,7 +1,14 @@
 public class Ball {
     String name;
+    int hitPoints;
 
     public Ball(String n){
         name = n;
+        hitPoints = (int)(Math.random() * 50 + 1);
+    }
+
+    public void sayHi(){
+        System.out.println("Hi, I'm " + name + "!");
+        System.out.println("I have " + hitPoints + " hit points.");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Ball ball = new Ball("ball1");
-        Ball ball2 = new Ball("ball2");
-        System.out.println(ball.name);
-        System.out.println(ball2.name);
+        ball.sayHi();
     }
 }
