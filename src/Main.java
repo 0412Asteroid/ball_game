6f436db2d70d6946d09666981b2ee65d98222352
0412Asteroid <chengyih258@edu.sd45.bc.ca>
2022-11-05 15:46:03 -0700

    added ball2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Ball ball = new Ball("ball1");
         ball.sayHi();
+        Ball ball2 = new Ball( "ball2");
+        ball2.sayHi();
     }
 }
