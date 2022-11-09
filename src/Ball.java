6b320daf47195d1a8e01387c5f2bad1d84ea2e66
0412Asteroid <chengyih258@edu.sd45.bc.ca>
2022-11-08 16:12:47 -0800

    added size, move

--- src/Ball.java
+++ src/Ball.java
@@ -1,27 +1,30 @@
 public class Ball {
     String name;
-    int hitPoints;
+    float x;
+    float y;
+    int size;
 
     public Ball(String n) {
         name = n;
-        hitPoints = (int) (Math.random() * 50 + 1);
+        x = 0;
+        y = 0;
+        size = (int) (Math.random() * 50 + 1);
     }
 
     public void sayHi() {
         System.out.println("Hi, I'm " + name + "!");
-        System.out.println("I have " + hitPoints + " hit points.");
+        System.out.println("I have " + size + " area.");
 
     }
 
     public void fight(Ball b) {
         Ball winner;
-        if (hitPoints > b.hitPoints) {
+        if (size > b.size) {
             winner = this;
         } else {
             winner = b;
         }
         System.out.println("The winner is " + winner.name + "!");
-        System.out.println("Win by " + Math.abs(hitPoints - b.hitPoints) + " points");
-
+        System.out.println("Win by " + Math.abs(size - b.size) + " points");
     }
 }
